76b2f8cb676e43e54f005428b2813d00d8f3dc2c
Reza <ahmad.musthafa@tokopedia.com>
2019-05-06 10:12:03 +0700

    Save final output to csv

--- core/type.go
+++ core/type.go
@@ -14,3 +14,112 @@ type Filter struct {
 	GrepType enum.GrepType
 	Value    string
 }
+
+type (
+	UseCodeInput struct {
+		Data UseCodeData `json:"data"`
+	}
+
+	UseCodeData struct {
+		Codes              []string               `json:"codes"`
+		CurrentApplyCode   UseCodeApplyInfo       `json:"current_apply_code"`
+		PromoCodeSessionID int64                  `json:"promo_code_id"`
+		UserData           UseCodeUserData        `json:"user_data"`
+		PaymentInfo        UseCodePaymentInfo     `json:"payment_info"`
+		MetaData           map[string]interface{} `json:"meta_data"`
+		GrandTotal         float64                `json:"grand_total"`
+		PaymentAmount      float64                `json:"payment_amount"`
+
+		// service id and secret key
+		ServiceID int64  `json:"service_id"`
+		SecretKey string `json:"secret_key"`
+
+		// promocode related
+		IsFirstStep bool `json:"is_first_step"`
+		Book        bool `json:"book"`
+		IsSuggested bool `json:"is_suggested"`
+		IsAppRoot   bool `json:"is_app_root"`
+		SkipApply   bool `json:"skip_apply"`
+
+		// rule engine related
+		Campaign []UseCodeCampaignData `json:"campaign_data"`
+
+		// after payment
+		PaymentID                int64 `json:"payment_id"`
+		FinishedOrderID          int64 `json:"finished_order_id"`
+		IsSkipCheckNotifySuccess bool  `json:"is_skip_check_notify_success"`
+
+		IsSkipFraudCheck bool      `json:"is_skip_fraud_check"`
+		FraudInfo        FraudInfo `json:"fraud_info"`
+
+		// others
+		Language string `json:"language"`
+		State    string `json:"state"`
+
+		//data recovery
+		DataRecoveryMode bool `json:"data_recovery_mode"`
+	}
+
+	UseCodeUserData struct {
+		UserID                int64                  `json:"user_id"`
+		Name                  string                 `json:"name"`
+		Email                 string                 `json:"email"`
+		Msisdn                string                 `json:"msisdn"`
+		MsisdnVerified        bool                   `json:"msisdn_verified"`
+		IsQcAccount           bool                   `json:"is_qc_acc"`
+		AppVersion            string                 `json:"app_version"`
+		UserAgent             string                 `json:"user_agent"`
+		IPAddress             string                 `json:"ip_address"`
+		AdsID                 string                 `json:"advertisement_id"`
+		DeviceType            string                 `json:"device_type"`
+		DeviceID              string                 `json:"device_id"`
+		AddressDetail         map[string]interface{} `json:"address_detail"`
+		UserTransactionDetail map[string]interface{} `json:"user_transaction_detail"`
+	}
+
+	UseCodePaymentInfo struct {
+		ScroogeGatewayID      int64   `json:"scrooge_gateway_id"`
+		ScroogeGatewayCode    string  `json:"scrooge_gateway_code"`
+		ScroogeGatewayValue   string  `json:"scrooge_gateway_value"`
+		CreditCardNumber      string  `json:"credit_card_number"`
+		CreditCardExpiryMonth int     `json:"cc_exp_month"`
+		CreditCardExpiryYear  int     `json:"cc_exp_year"`
+		CreditCardHash        string  `json:"cc_hash"`
+		OvoCashAmount         float64 `json:"ovo_cash_amount"`
+		OvoPointsAmount       float64 `json:"ovo_points_amount"`
+	}
+
+	UseCodeCampaignData struct {
+		Code        string                 `json:"code"`
+		PromoCodeID int64                  `json:"promo_code_id"`
+		PromoID     int64                  `json:"promo_id"`
+		RuleIDs     []int64                `json:"rule_ids"`
+		DoGaladriel []string               `json:"do_galadriel"`
+		FlowState   int                    `json:"flow_state"`
+		MetaData    map[string]interface{} `json:"meta_data"`
+	}
+
+	PromoStackAbuse struct {
+		PromoID       int64 `json:"promo_id"`
+		IsPromoAbuser int   `json:"is_promo_abuser"`
+	}
+
+	UseCodeServiceData struct {
+		CategoryCode int    `json:"category_code"`
+		ProductCode  string `json:"product_code"`
+	}
+
+	UseCodeApplyInfo struct {
+		Code string `json:"code"`
+		Type string `json:"type"`
+	}
+)
+
+type FraudInfo struct {
+	DropshipAsBuyer int               `json:"dropship_as_buyer"`
+	IsPromoAbuser   int               `json:"is_promo_abuser"`
+	IsInvalid       int               `json:"is_invalid"`
+	Status          interface{}       `json:"status"`
+	PromoStackAbuse []PromoStackAbuse `json:"promo_stack_abuse"`
+	Source          string            `json:"source"`
+}
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/json-iterator/go"
 	. "github.com/tokopedia/sweep-log/core"
 	"github.com/tokopedia/sweep-log/core/enum"
 	"log"
+	"marisinau.com/KitaUndangAPI/util/errors"
+	"math"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -38,12 +41,17 @@ func main() {
 		return
 	}
 
+	if err := processOutput(); err != nil {
+		return
+	}
+
 	fmt.Println("\n=== FINISH YOOOOOOOOOOOO CAKKK !!! ===")
 }
 
 func deleteOutputFile() {
 	DeleteOutputFile(GENERATED_VALIDATE_USE_FILENAME)
 	DeleteOutputFile(GENERATED_NOTIFY_SUCCESS_FILENAME)
+	DeleteOutputFile(GENERATED_CSV_FILENAME)
 }
 
 func readInputOrderFile() error {
@@ -62,6 +70,10 @@ func readInputOrderFile() error {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		rowStr := scanner.Text()
+
+		if rowStr == "" {
+			continue
+		}
 
 		rArr := strings.Split(rowStr, "|")
 		if len(rArr) > 0 {
@@ -117,7 +129,7 @@ func getNotifySuccessData() error {
 	filters := []Filter{
 		{GrepType: enum.GrepStandard, Value: "NOTIFY USE SUCCESS"},
 		{GrepType: enum.GrepCombined, Value: DelimSliceToString(PaymentIDs, "|")},
-		{GrepType: enum.GrepStandard, Value: "2019-04-29"},
+		//{GrepType: enum.GrepStandard, Value: "2019-04-29"},
 	}
 
 	for i, logPath := range LogPaths {
@@ -136,12 +148,73 @@ func getValidateUseData() error {
 		{GrepType: enum.GrepStandard, Value: "VALIDATE USE"},
 		{GrepType: enum.GrepCombined, Value: DelimSliceToString(PromoCodes, "|")},
 		{GrepType: enum.GrepStandard, Value: "\\\\\"book\\\\\":true"},
-		{GrepType: enum.GrepStandard, Value: "2019-04-29"},
+		//{GrepType: enum.GrepStandard, Value: "2019-04-29"},
 	}
 
 	for i, logPath := range LogPaths {
 		fmt.Printf("=== Processing VU [%d:%d] %s\n", (i + 1), len(LogPaths), logPath)
 		GenerateCommand(logPath, filters, VALIDATE_USE, MODE_ALL_IN_ONE)
+	}
+
+	return nil
+}
+
+func processOutput() error {
+
+	if !IsOutputFileExist(GENERATED_NOTIFY_SUCCESS_FILENAME) && !IsOutputFileExist(GENERATED_VALIDATE_USE_FILENAME) {
+		return errors.New(fmt.Sprintf("Failed, make sure %s and %s is exist"), GENERATED_NOTIFY_SUCCESS_FILENAME, GENERATED_VALIDATE_USE_FILENAME)
+	}
+
+	fmt.Println("\n=== Process final output ===")
+
+	file, err := os.Open(GENERATED_OUTPUT_DIR + GENERATED_NOTIFY_SUCCESS_FILENAME)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := scanner.Text()
+
+		if row == "" {
+			continue
+		}
+
+		var nsRow UseCodeInput
+		err := jsoniter.Unmarshal([]byte(row), &nsRow)
+		if err != nil {
+			fmt.Println("PROBLEM WITH DATA NS:", row)
+			continue
+		}
+
+		filters := []Filter{
+			{GrepType: enum.GrepStandard, Value: fmt.Sprintf("\"user_id\":%d", nsRow.Data.UserData.UserID)},
+			{GrepType: enum.GrepStandard, Value: fmt.Sprintf("\"grand_total\":%.f", math.Round(nsRow.Data.PaymentAmount))},
+		}
+
+		vuOutput := GenerateCommandFinalProcess(GENERATED_OUTPUT_DIR+GENERATED_VALIDATE_USE_FILENAME, filters)
+		if vuOutput != "" {
+			vuArr := strings.Split(vuOutput, "\n")
+			for _, vu := range vuArr {
+
+				if vu == "" {
+					continue
+				}
+
+				var vuRow UseCodeInput
+				err := jsoniter.Unmarshal([]byte(vu), &vuRow)
+				if err != nil {
+					fmt.Println("PROBLEM WITH DATA VU:", vu)
+					continue
+				}
+
+				if nsRow.Data.UserData.UserID == vuRow.Data.UserData.UserID && nsRow.Data.PaymentAmount == vuRow.Data.GrandTotal {
+					AppendToFile(GENERATED_CSV_FILENAME, fmt.Sprintf("%s~~~~~%s\n", row, vu))
+					break
+				}
+			}
+		}
 	}
 
 	return nil
